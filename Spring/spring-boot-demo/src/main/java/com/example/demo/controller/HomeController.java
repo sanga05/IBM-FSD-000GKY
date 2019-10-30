5a9237f1edca38b1ad61699c26d898d9b87037e5
sanga05 <spamme017@yandex.com>
2019-10-30 13:33:35 +0000

    Spring demo

--- Spring/spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
+++ Spring/spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -12,46 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Entity.Customer;
-import com.example.demo.Services.CustomerServices;
-
 @RestController
 @RequestMapping("/")
 public class HomeController {
 
-	@Autowired
-	private CustomerServices customerServices;
 
 	@GetMapping("/index")
 	public String Home() {
 		return "Hello";
 	}
 
-	@GetMapping("/customer")	
-	public List<Customer> getCustomer() {
-		return customerServices.getCustomers();
-	}
-	@PostMapping("/customer")
-	public Customer addCustomers(@RequestBody Customer customer) {
-		
-		customerServices.updateCustomer(customer);
-		return customer;
-		
-	}
-	@DeleteMapping("/customer/{id}")
-	public boolean deleteCustomer(@PathVariable int id) {
-	customerServices.deleteCustomer(id);
-		return true;
-		
-	}
-	@PutMapping("/customer")
-	public boolean updateCustomer(@RequestBody Customer customer) {
-		customerServices.updateCustomer(customer);
-		return true;
-	}
-	
-	@GetMapping("/customer/{id}")
-	public Customer getById(@PathVariable int id) {
-		return customerServices.getByID(id);
-	}
 }
